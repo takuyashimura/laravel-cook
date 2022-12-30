afbfe45a9fb50bcfeae1f247b45f8b4e03a9e102
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2022-12-30 17:41:06 +0900

    no message

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Food;
+use App\Models\Menu;
+use APP\Models\Stock;
 
 class HomeController extends Controller
 {
@@ -25,14 +28,47 @@ class HomeController extends Controller
      //食材画面
     public function index()
     {
-        return view('create');
+        //ここで食材名を取得する
+        $food = Food::select('food.*')
+        ->where('user_id', '=' , \Auth::id())
+        ->whereNull('deleted_at')
+        ->orderby('created_at','DESC')
+        ->get();
+        //在庫数を取得
+        // $stocks= Stock::select('stock.*')
+        // ->where('food_id' , '=' , \Food::id())
+        
+
+        return view('create' , compact('food'));
     }
 
     //メニュー画面
     public function menu()
     {
-        return view('menu');//create→食材画面
+        //ここでメニュー名を取得
+        $menus= Menu::select('menus.*')
+        ->where('user_id' , '=' , \Auth::id())
+        ->whereNull('deleted_at')
+        ->orderby('created_at','DESC')
+        ->get();
+        
+
+        return view('menu',compact('menus'));
     }
+    //メニュー画面
+    public function add_food()
+    {
+       return view('add_food');  
+    }
+    //食材を追加した時の処理
+    public function add(Request $request)
+    {
+        $posts=$request->all();
+        Food::insert(['name' => $posts['name'] , 'user_id' => \Auth::id()]);
+
+        return redirect( route('home') );  
+    }
+
 
 }
 
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -4,10 +4,16 @@
         <div class='blue_elea rounded-pill w-25' style='text-align:center'>
         メニュー追加
         </div>
-        <div class='blue_elea d-flex justify-content-between'>
-            <div>メニュー名</div>
-            <div>在庫数</div>
-        </div>
+        @foreach ($menus as $menu)
+        <div class='blue_elea'>
+            <p class='text-center'>{{ $menu['name']}}</p>
+            <div class='d-flex justify-content-end'>
+                <p>削除</p>
+                <p>調理</p>
+                <p>編集</p>
+            </div>
+        @endforeach
+        <!-- </div>
         <div class='blue_elea d-flex justify-content-between'>
             <div>メニュー名</div>
             <div>在庫数</div>
@@ -19,5 +25,5 @@
         <div class='blue_elea d-flex justify-content-between'>
             <div>メニュー名</div>
             <div>在庫数</div>
-        </div>
+        </div> -->
 @endsection
