93137629df9cbf141bb340c512eefe3524c04d68
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-01-13 11:33:55 +0900

    食材追加画面を新規食材登録のみの昨日にした。今後user_idのカラムを持ったfoodテーブルを作成したらこの画面を使えるようになる。初期はデフォルトで登録されている食材の在庫管理しかできないものなので、user_idのカラムを持ったfoodテーブルを追加しない限り稼働するコードではない

---
+++ resources/views/add_food.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.finely')
+
+@section('content')
+    <form  action='{{ route('add') }}' method='POST'>
+        @csrf
+        <!-- 食材入力部分 -->
+        <div class='text-center'>
+            <input type="text" class='w-50' name='name' placeholder="登録する食材名">
+        </div>
+         <!-- 食材を在庫加えるボタン-->
+         <div class='text-center'>
+            <button  type='submit'>新しい食材を<br>追加</button>
+         </div>
+        </div>
+
+            
+                    
+        </div>
+        
+    </fomr>
+
+@endsection
+    
