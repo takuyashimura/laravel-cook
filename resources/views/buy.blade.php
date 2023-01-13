71a1b5e62a16b2c00200f51d1c53370540f60a96
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-01-14 00:47:18 +0900

    購入画面の編集 食材名表示のブロック要素内に追加する在庫数を記入できるようにした また処理を実行するための追加ボタンも作成

--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -10,10 +10,15 @@
                         {{ $food['name']}}
                      </p>
                 </a>
+                <input type="number" name='amount' min='0' max='100'><!--stocksのamountに数字を追加-->
              </div>
+
         @endforeach
         <!-- <div class='blue_elea'>食材名</div>
         <div class='blue_elea'>食材名</div>
         <div class='blue_elea'>メロン</div> -->
     </div>
+    <div class='text-center'>
+            <button  type='submit'>新しい食材を<br>追加</button>
+         </div>
 @endsection
