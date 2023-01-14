a4d430af5e1c7711a7d849fedf3cadd2241a6a8b
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-01-14 21:51:09 +0900

    foreach文を同じディレクトリに入れえるとコメントアウトしてもエラーになってしまう。なので、エラーになってしまうコードをsubとして保存しておき、それ以降のコーディングを今まで通りbuyディレクトリにて進めていく

--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -1,24 +1,20 @@
 @extends('layouts.finely')
 
 @section('content')
-<div>
-        <div class='blue_elea' style='text-align:center rounded-pill'>新しい食材を<br>追加</div>
+    <div>
+       
+        <!-- 以下udemy参考コード -->
         @foreach($food as $food)
             <div class='blue_elea'>
-                <a href="">
-                    <p>
-                        {{ $food['name']}}
-                     </p>
-                </a>
-                <input type="number" name='amount' min='0' max='100'><!--stocksのamountに数字を追加-->
-             </div>
-
-        @endforeach
+               <p>{{ $food['name'] }}</p>
+            </div>
+        @endforeach 
+       
         <!-- <div class='blue_elea'>食材名</div>
         <div class='blue_elea'>食材名</div>
         <div class='blue_elea'>メロン</div> -->
     </div>
-    <div class='text-center'>
-            <button  type='submit'>新しい食材を<br>追加</button>
-         </div>
+        <div class='text-center'>
+                <button  type='submit'>新しい食材を<br>追加</button>
+        </div>
 @endsection
