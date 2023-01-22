7d6dd9b33f2fb86a9e003a6893c4eb8743ff74b9
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-01-22 23:55:58 +0900

    食材名の横に在庫数が表示されるようにコーディング

--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -5,14 +5,16 @@
        
         <!-- 以下udemy参考コード -->
         @foreach($food as $food)
-            <div class='blue_elea'>
+            @foreach($food->stocks as $stock) 
+            <div class='blue_elea d-flex justify-content-between '>
                <p>{{ $food['name'] }}</p>
+               <p>{{ $stock['amount'] }}</p>
+               <input type="number" name='amount' min='0' max='100'>
+
             </div>
+            @endforeach 
         @endforeach 
-       
-        <!-- <div class='blue_elea'>食材名</div>
-        <div class='blue_elea'>食材名</div>
-        <div class='blue_elea'>メロン</div> -->
+
     </div>
         <div class='text-center'>
                 <button  type='submit'>新しい食材を<br>追加</button>
