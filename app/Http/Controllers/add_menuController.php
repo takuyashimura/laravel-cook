fe520d3c5083b1edc0b637d2b566000987d60e00
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-01-27 17:27:06 +0900

    メニュー追加画面作成に際して追加されたディレクトリ

---
+++ app/Http/Controllers/add_menuController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Food;
+use App\Models\Menu;
+use App\Models\Stock;
+use App\Models\User;
+use DB;
+
+class add_menuController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+
+     
+
+    // 食材購入画面
+    public function add_menu()
+    {
+        $food = Food::select('food.*')
+        ->orderby('created_at','DESC')
+        ->get();
+
+
+        
+       return view('add_menu',compact("food"));  
+    }
+}
+
+
