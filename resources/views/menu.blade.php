c6899c8bed74327bcdc5f056a3069c64a0c5455b
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-01-27 17:26:12 +0900

    メニュー画面の編集。DBにあるメニュー名を取得し表示できるようにした。

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -2,15 +2,20 @@
 
 @section('content')
         <div class='blue_elea rounded-pill w-25' style='text-align:center'>
-        メニュー追加
+        <a href="http://localhost:8888/add_menu">
+            メニュー追加
+        </a>
         </div>
         @foreach ($menus as $menu)
-        <div class='blue_elea'>
-            <p class='text-center'>{{ $menu['name']}}</p>
+        <div class='blue_elea mt-1'>
+            <a href="http://localhost:8888/menu_cook">
+                <p class='text-center'>{{ $menu['name']}}</p>
+            </a>    
+            
             <div class='d-flex justify-content-end'>
                 <p class='bg-secondary text-white' style='margin:0px 5px 0px'>削除</p>
-                <p class='bg-secondary text-white' style='margin:0px 5px 0px'>調理</p>
                 <p class='bg-secondary text-white' style='margin:0px 5px 0px'>編集</p>
             </div>
+        </div>
         @endforeach
 @endsection
