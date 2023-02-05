501bd4fb1e5fc4016049cc131aa4db6b15184bee
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-02-05 15:54:00 +0900

    no message

---
+++ resources/js/php_study_4/page9/user.php
@@ -0,0 +1,19 @@
+<?php
+// Userクラスを定義してください
+class User {
+  private $name;
+  private $gender;
+  
+  public function __construct($name,$gender){
+    $this -> name = $name;
+    $this -> gender = $gender;
+  }
+  public function getName(){
+    return $this ->name;
+  }
+    public function getGender(){
+    return $this -> gender;
+  }
+}
+
+?>
