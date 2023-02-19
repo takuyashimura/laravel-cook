658ad6ff6d51c10a86d6cb368511a71f372696de
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-02-19 15:31:06 +0900

    調理リストの実装コード。まだ未完成

--- resources/views/cooking_list.blade.php
+++ resources/views/cooking_list.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-   <p>クッキングリストだお</p>
+
+   @foreach($cooking_lists as $cooking_list)
+      <div class='blue_elea ' >   
+         <p>メニュー名→{{$menus[$cooking_list->menu_id]->name}}</p>
+         <p>削除</p>
+      </div>
+   @endforeach
 @endsection
