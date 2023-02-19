d87c2183019fdb2c12998240347d9f50cd42b56a
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-02-19 15:29:46 +0900

    DB修正に伴い、food_menusテーブルのカラムの追加ファイルの作成

---
+++ database/migrations/2023_02_18_155136_add_id_to_food_menus_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIdToFoodMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('food_menus', function (Blueprint $table) {
+            $table->unsignedBigInteger('id',true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('food_menus', function (Blueprint $table) {
+            $table->dropColumn('id');  
+        });
+    }
+}
