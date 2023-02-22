d5499c55608388d387a4e89ba4422fd404defaa6
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-02-22 22:19:33 +0900

    食材画面からその食材を使用するメニューを表示する機能の実装

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,18 +5,22 @@
         <div class='blue_elea' style='text-align:center rounded-pill'>新しい食材を<br>追加</div>
         <!-- aタグのhrefを使って食材をクリックするとその食材を使うメニューが表示されるようにする -->
         @foreach($stocks as $stock) 
-            <div class='blue_elea d-flex justify-content-between '>
-                <p>{{ $food[$stock->food_id]->name }}</p>
-                <p>{{ $stock['total_amount'] }}</p>
-            </div>
+            <a href="{{route('foodToMenu' ,$food_id = $stock->food_id)}}">
+                <div class='blue_elea d-flex justify-content-between '>
+                    <p>{{ $food[$stock->food_id]->name }}</p>
+                    <p>{{ $stock['total_amount'] }}</p>
+                </div>
+            </a>
         @endforeach 
 
         <!-- stocksテーブルにfood_idがない→在庫がないと判断する -->
         @foreach($array as $array)
-        <div class='blue_elea d-flex justify-content-between '>
-                <p>{{ $food[$array]->name }}</p>
-                <p>0</p>
-            </div>
+            <a href="{{route('foodToMenu' ,$food_id = $array)}}">
+                <div class='blue_elea d-flex justify-content-between '>
+                    <p>{{ $food[$array]->name }}</p>
+                    <p>0</p>
+                </div>
+            </a>
         @endforeach
         
         <!-- stocksターブルにidはあるが在庫数が0の時のコーディングをする -->
