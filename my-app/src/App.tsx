333994eabda08d8b13101b09169f48984b5923a5
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-03-11 14:12:23 +0900

    type.script環境構築

---
+++ my-app/src/App.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const App = () => {
+    const [value, setValue] = useState([]);
+
+    // 先ほど作成したLaravelのAPIのURL
+    const url = "http://localhost:8888/api/home";
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await axios.get(url);
+                setValue(res.data.post);
+                return;
+            } catch (e) {
+                return e;
+            }
+        })();
+    }, []);
+
+    return (
+        <div className="App">
+            {value.map((article) => {
+                return (
+                    <div key={article.id}>
+                        <h1>{article.title}</h1>
+                        <p>{article.content}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default App;
