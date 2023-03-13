77a224a497ff894d46e40a06ce100d2e4eca5fcc
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-03-13 14:12:16 +0900

    質問の該当コード。どこが関与しているか、自分で判断すると不足分が出てしまうと考えたので、追加したファイルを全てコミットに含めました

---
+++ my-app/src/components/primarybuttom.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "@emotion/styled";
+// import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export const primaryButtom = (props: string) => {
+    const { children } = prpps;
+    return;
+    <>
+        <Link to="#">{children}</Link>
+    </>;
+};
---
+++ my-app/src/css/css.tsx
@@ -0,0 +1,9 @@
+import { css } from "@chakra-ui/react";
+
+export const Footer = css({
+    display: "flex",
+    position: "fixed",
+    bottom: "0",
+    width: "100%",
+    backgroundColor: "blue",
+});
