287470e4f6e0563cd4d5408e8ecc53087ec3f9f7
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-04-10 13:17:51 +0900

    コンフリクトチェック

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,10 @@ use App\Http\Controllers\addBuyListByCoookingListController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Route::get('/{any}', function () {
+    return view('index');
+})->where('any', '.*');
+
 
 
 // いいいいい
