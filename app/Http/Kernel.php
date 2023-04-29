db0236e579276099803aa125edbdf8e9b1c5d6eb
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-04-29 10:45:49 +0900

    corsエラー解決を試みる

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,7 +67,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => \Barryvdh\Cors\HandleCors::class,
+        'cors' => \App\Http\Middleware\Cors::class,
 
     ];
 }
---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+        ->header('Access-Control-Allow-Origin', 'https://react-my-app-eight.vercel.app')
+        ->header('Access-Control-Allow-Credentials','true')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', '*');
+    }
+}
